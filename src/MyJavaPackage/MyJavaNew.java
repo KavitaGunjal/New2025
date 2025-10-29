dd095bd3bc2135a7c40a5d1e68e48e695eecbbcb
kavit <kavit@192.168.1.4>
2025-10-29 13:48:25 +0530

    First Commit ......

---
+++ src/MyJavaPackage/MyJavaNew.java
@@ -0,0 +1,17 @@
+package MyJavaPackage;
+
+public class MyJavaNew {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    
+
+	}
+
+}
